414aad1378af6f6c4f92fd50910244688dda23c4
vidhu-ops <vidhugupta1996@gmail.com>
2026-08-07 16:57:49 +0530

    Add one-day delivery rules and mobile app support

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'get-it-done-shell-v1';
+const SHELL = ['/', '/index.html', '/manifest.webmanifest', '/assets/app-icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then(cached => cached || caches.match('/index.html'))));
+});
